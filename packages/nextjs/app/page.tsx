1ceb389219cf65d1c8fcf004d003e4eb6819b017
Rikzon12 <134078015+Rikzon12@users.noreply.github.com>
2026-05-22 08:35:45 +0700

    page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -16,21 +16,21 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-2xl mb-2">Powering Defi on LitVM</span>
+            <span className="block text-4xl font-bold">SAM FINANCE 2</span>
           </h1>
           <div className="flex justify-center items-center space-x-2 flex-col">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} chain={targetNetwork} />
           </div>
           <p className="text-center text-lg">
-            Get started by editing{" "}
+            Trade, Stake, and Provide Liquidity on SAM FINANCE{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               packages/nextjs/app/page.tsx
             </code>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
+            Multi-chain DeFi ecosystem powered by LitVM{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               YourContract.sol
             </code>{" "}
