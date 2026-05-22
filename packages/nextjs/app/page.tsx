62d6c2d291fe985bb03517a83e0151cb778bcd6d
Rikzon12 <134078015+Rikzon12@users.noreply.github.com>
2026-05-22 20:17:48 +0700

    page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           </button>
 
           {/* CONNECT WALLET */}
-          <appkit-button />
+          <button>Connect Wallet</button>
 
         </div>
 
