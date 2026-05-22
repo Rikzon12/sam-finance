f3185e8f1385fad9ea5118657ddebe39a6f1bee6
Rikzon12 <134078015+Rikzon12@users.noreply.github.com>
2026-05-22 10:08:01 +0700

    scaffold.config.ts

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,34 +1,6 @@
 import * as chains from "viem/chains";
-import { defineChain } from "viem";
+import { litvmLiteForge } from "~~/utils/scaffold-eth/chains";
 
-/* =========================
-   LITVM CHAIN
-========================= */
-const litvmLiteForge = defineChain({
-  id: 4441,
-  name: "LitVM LiteForge",
-  nativeCurrency: {
-    name: "zkLTC",
-    symbol: "zkLTC",
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: ["https://liteforge.rpc.caldera.xyz/http"],
-      webSocket: ["wss://liteforge.rpc.caldera.xyz/ws"],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: "LiteForge Explorer",
-      url: "https://liteforge.explorer.caldera.xyz",
-    },
-  },
-});
-
-/* =========================
-   CONFIG TYPE
-========================= */
 export type ScaffoldConfig = {
   targetNetworks: readonly chains.Chain[];
   pollingInterval: number;
@@ -38,9 +10,6 @@ export type ScaffoldConfig = {
   burnerWalletMode: "localNetworksOnly" | "allNetworks" | "disabled";
 };
 
-/* =========================
-   DEFAULT CONFIG
-========================= */
 export const DEFAULT_ALCHEMY_API_KEY = "IZYEU2cWBgnFmgiTAgpWD";
 
 const scaffoldConfig = {
